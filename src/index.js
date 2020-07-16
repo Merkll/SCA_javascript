9a93faba28a430c226402de9d5df9211f488482c
Toluwase <toluwase103@gmail.com>
2020-07-16 18:21:49 +0100

    asychronous programming js

--- src/index.js
+++ src/index.js
@@ -1,82 +1,122 @@
- import faker from 'faker'
-import Student from './Student'
-import Teacher from './Teacher'
-import Course from './Course'
-import Courses from './Courses'
-//  import { prompt } from 'inquirer'
+const arr1 = [1,2,3,4,5];
 
-const courseManager = new Courses()
+function delay(time = 5000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(0);
+    } , time)
+  })
+}
 
- const students = [
-   {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: faker.random.number(20)
-  },
-  {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: faker.random.number(20)
-  },
-  {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: faker.random.number(20)
-  },
-  {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: faker.random.number(20)
+function checkBalance(accountNumber, amount) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("checking....")
+      if (accountNumber === 0) return resolve(1000,amount);
+
+      resolve(0);
+    } , 5000)
+  })
+}
+
+async function checkBalanceasync(accountNumber) {
+  await delay(Math.random() * 10000);
+
+  return accountNumber === 0 ? 1000 : 0
+}
+
+
+function withdraw(amount, balance) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("withdrawing....")
+      if (amount < balance) return resolve(true);
+
+      reject("Not enough balance");
+    } , 3000)
+  })
+}
+
+async function withdrawsync(balance, amount) {
+  await delay(Math.random() * 10000)
+
+  if (amount > balance) throw new Error("insuffienct")
+  return amount < balance
+}
+
+function dispenceCash(amount, canDispence) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("dispensing....")
+      if(canDispence) return resolve(amount);
+
+      reject("Error")
+    } , 6000)
+  })
+}
+
+async function dispenceCashsync(canDispence, amount) {
+  await delay(Math.random() * 10000);
+
+  return amount
+}
+
+
+function customerAction(identifier) {
+  return new Promise((resolve, reject) => {
+    console.log("executing for => ", identifier)
+    checkBalance(0)
+    .then((balance) => withdraw(balance, 300))
+    .then((dispence) => dispenceCash(dispence, 300))
+    .then(() => console.log("done executing for => ", identifier))
+    .then(resolve)
+    .catch((err) => console.log("ATM error =>", err))
+  })
+
+}
+
+async function customerActionsync(identifier) {
+  try {
+    console.log("Excuting =>", identifier)
+    const balance = await checkBalanceasync(0);
+    console.log("Checked balance =>", identifier, balance)
+    const shouldDispence = await withdrawsync(balance, 300)
+    console.log("withdraw =>", identifier)
+    await dispenceCashsync(shouldDispence, 300)
+    console.log("dispensed =>", identifier)
+  } catch (error) {
+    console.log(error)
   }
-]
+}
 
-const teachers = [
-  {
-   firstName: faker.name.firstName(),
-   lastName: faker.name.lastName(),
-   age: faker.random.number(20)
- },
- {
-   firstName: faker.name.firstName(),
-   lastName: faker.name.lastName(),
-   age: faker.random.number(20)
- },
- {
-   firstName: faker.name.firstName(),
-   lastName: faker.name.lastName(),
-   age: faker.random.number(20)
- },
- {
-   firstName: faker.name.firstName(),
-   lastName: faker.name.lastName(),
-   age: faker.random.number(20)
- }
-]
-
-courseManager.createCourse('math')
-courseManager.createCourse('english')
-
-const CoursesEnum = Object.freeze({
-  physis: 'physics',
-  math: 'maths'
-})
-
-courseManager.createCourse(CoursesEnum.math)
-courseManager.createCourse(CoursesEnum.physis)
+customerAction("first")
+customerAction("second")
+customerAction("third")
 
 
-// student registration
-const student1 = new Student(students[0])
+checkBalance(0)
+.then((balance) => withdraw(balance, 300))
+.then((dispence) => dispenceCash(dispence, 300))
+.catch((err) => console.log("ATM error =>", err))
 
-student1.printScore()
-student1.registerCourse(Courses.math)
-student1.registerCourse(Courses.physis)
-student1.printScore()
+checkBalance(0)
+  .then((balance) => {
+    withdraw(balance, 300)
+    .then((dispence) => {
+      dispenceCash(dispence, 300)
+    })
+  })
 
-const mathTeacher = new Teacher(teachers[0])
-mathTeacher.assignCourse(Courses.math)
-mathTeacher.assignCourse(Courses.physis)
-mathTeacher.gradeScore(student1, Courses.math, 50);
-mathTeacher.gradeScore(student1, Courses.physis, 70);
+checkBalance(0)
+  .then(withdraw.bind(null, 300))
+  .then(dispenceCash.bind(null, 300))
+  .catch((err) => console.log("ATM error =>", err))
 
-student1.printScore()
+
+async function notify() {
+  const data = await Promise.all([customerActionsync("first"), customerActionsync("second"), customerActionsync("third")])
+  console.log("Notifing admin")
+
+}
+
+notify()
